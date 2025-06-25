bc0d2a1da442a2815f9cb4702c2c385fbbfe38d3
Guilherme <guilhermesaar02@gmail.com>
2025-06-25 19:48:32 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Controle de Assinatura (em desenvolvimento)
---
+++ src/test/java/com/gstech/controle_assinatura/ControleAssinaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstech.controle_assinatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAssinaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
